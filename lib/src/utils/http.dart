3911a358825014b460dacd547eaf295c7be13d62
Israel Lugo <hostelixisrael@gmail.com>
2019-02-27 11:25:22 -0400

    [ADD] add utils http

---
+++ lib/src/utils/http.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+Map<String, String> buildBearerAuthorizationHeader(String token) {
+  return {'authorization': 'Bearer $token'};
+}
+
+Map<String, String> buildBasicAuthorizationHeader(username, password) {
+  String credentials = base64Encode(utf8.encode('$username:$password'));
+  return {'authorization': 'Basic $credentials'};
+}
+
+String normalizeURI(String uri) {
+  return uri[0] != '/' ? uri : uri.substring(1);
+}
+
+String buildUrl(String baseUrl, String uri, {String version = '', Map<String, String> queryParams) {
+  String resource = version.isNotEmpty ? '$uri/$version' : uri;
+  return Uri.https(baseUrl, normalizeURI(resource), queryParams).toString();
+}
+
+String buildBody(Map<String, String> data){
+  return data.keys.map((String key) => "${Uri.encodeComponent(key)}=${Uri.encodeComponent(data[key])}").join("&");
+}
