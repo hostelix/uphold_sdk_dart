4b3d36945c6b8f86267d67091e6a9ed443adff4b
Israel Lugo <hostelixisrael@gmail.com>
2019-02-28 14:54:02 -0400

    [MOD] add sdk option class to SDK class

--- lib/src/sdk.dart
+++ lib/src/sdk.dart
@@ -1,23 +1,81 @@
-import '../src/services/oauth-client.dart' show OAuthClient;
+import 'package:http/http.dart' show Request, Response;
+
+import '../src/services/oauth_client.dart' show OAuthClient, TokenRequest;
+import '../src/services/request_client.dart';
+import '../src/version.dart' show SDK_VERSION;
 
 class SDK {
-  Map<String, dynamic> defaultOptions = {
-    'accessTokenKey': 'uphold.access_token',
-    'baseUrl': 'api.uphold.com',
-    'itemsPerPage': 10,
-    'refreshTokenKey': 'uphold.refresh_token',
-    'version': 'v0'
-  };
   OAuthClient _oauthClient;
+  RequestClient _client;
+  SDKOptions _options;
 
-  Map<String, dynamic> options;
-
-  SDK(
-    String clientId,
-    String clientSecret, {
-    String accessTokenKey,
-    int itemsPerPage,
-  }) {
-    _oauthClient = OAuthClient('baseUrl', 'clientId', 'clientSecret');
+  SDK(String clientId, String clientSecret, SDKOptions options) {
+    if (options == null) {
+      _options = SDKOptions();
+    }
+    _oauthClient = OAuthClient(_options.baseUrl, clientId, clientSecret);
+    _client = RequestClient('Uphold Dart SDK v$SDK_VERSION');
   }
+
+  Future<Response> authorize(code) {
+    final req =
+        _oauthClient.buildAccessTokenRequestByAuthorizationCodeGrant(code);
+
+    return _authenticationRequest(req);
+  }
+
+  Future<Response> _authenticationRequest(Request req) {
+    return _client.request(req);
+  }
+
+  /*Future<Response> _refreshToken(url, method, body, headers, options) {
+    return _requestRefreshToken
+        .then(tokens => {
+          return this.client.request(url, method, body, {
+            ...buildBearerAuthorizationHeader(tokens.access_token),
+            ...headers
+          }, options)
+            .then(data => data.body);
+        });
+    };
+  }*/
+
+  Future<Response> _requestRefreshToken(token) {
+    Request req = _oauthClient.buildRefreshTokenRequest(token);
+
+    return _authenticationRequest(req);
+  }
+
+  Future<Response> _revokeToken(String token) {
+    Request req = _oauthClient.buildRevokeTokenRequest(token);
+
+    return _client.request(req);
+  }
+}
+
+class SDKOptions {
+  String _accessTokenKey;
+  String _baseUrl;
+  int _itemsPerPage;
+  String _refreshTokenKey;
+  String _version;
+
+  SDKOptions(
+      {String accessTokenKey = "uphold.access_token",
+      String baseUrl = "https://api.uphold.com",
+      int itemsPerPage = 10,
+      String refreshTokenKey = "uphold.refresh_token",
+      String version = "v0"}) {
+    _accessTokenKey = accessTokenKey;
+    _baseUrl = baseUrl;
+    _itemsPerPage = itemsPerPage;
+    _refreshTokenKey = refreshTokenKey;
+    _version = version;
+  }
+
+  String get accessTokenKey => _accessTokenKey;
+  String get baseUrl => _baseUrl;
+  int get itemsPerPage => _itemsPerPage;
+  String get refreshTokenKey => _refreshTokenKey;
+  String get version => _version;
 }
